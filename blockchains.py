fd5b1b68e2d74127003faa7c438228aeb8e9e76a
Jöran Roslund <emwjros@gmail.com>
2019-03-13 08:07:36 +0100

    WIP. Session two

--- blockchains.py
+++ blockchains.py
@@ -1,8 +1,6 @@
 import hashlib 
-from time import time 
-import Crypto.PublicKey.RSA as RSA 
-from Crypto.Signature import pkcs1_15 
-from Crypto.Hash import SHA256 
+from time import time
+import Crypto.PublicKey.RSA as RSA
 
 class Wallet:
 
@@ -10,59 +8,79 @@ class Wallet:
         self.key = RSA.generate(1024) 
         self.public_key = self.key.publickey()
 
-    def make_transaction(self, data):
-        key_signer = pkcs1_15.new(self.key)
-        digest = SHA256.new()
-        digest.update(data)
-        return Transaction(data, self.public_key, key_signer.sign(digest))
+    def make_transaction(self, receiver, amount):
+        return Transaction(self.public_key, receiver, amount)
 
     def find_my_transactions(self, chain):
         number_of_transactions = 0
         for b in range(1, chain.number_of_blocks()):
-            if chain.blocks[b].transaction.public_key == self.public_key:
+            if chain.blocks[b].transaction.sender == self.public_key:
                 number_of_transactions += 1
         return number_of_transactions
-                
 
 class Transaction:
 
-    def __init__ (self, data, public_key, signature):
-        self.data = data
-        self.public_key = public_key
-        self.signature = signature
+    sequence_number = 0
 
-    def verify_signature(self):
-        digest = SHA256.new()
-        digest.update(self.data)
-        try:
-            pkcs1_15.new(self.public_key).verify(digest, self.signature)
-            return True
-        except ValueError:
-            return False
-        
+    def __init__ (self, sender, receiver, amount):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.sequence_number = Transaction.sequence_number
+        self.transaction_id = hashlib.sha256(self.data().encode()).hexdigest()
+        Transaction.sequence_number += 1
+
+    def data(self):
+        return str(self.sender) + str(self.receiver) + str(self.amount) + \
+                  str(self.sequence_number)
+
+    def verify(self):
+        return self.transaction_id == hashlib.sha256(
+            self.data().encode()).hexdigest()
+
 class Block: 
     
-    def __init__(self, transaction, previous_hash):           
-        self.previous_hash = previous_hash 
-        self.transaction = transaction 
-        self.time_of_creation = time() 
-        self.hash_of_self = self.hash()
+    def __init__(self, transactions, bank, miner):           
+        self.time_of_creation = time()
+        self.data = transactions
+        self.data.append(Transaction(bank, miner, 1.0))
+        self.nonce = 0
+        self.hash_of_self = ""
  
     def hash(self): 
-        contents = str(self.previous_hash) + str(self.transaction) + \
-                   str(self.time_of_creation)
+        contents = str(self.previous_hash) + str(self.data) + \
+                   str(self.time_of_creation) + str(nonce)
         return hashlib.sha256(contents.encode()).hexdigest()
         
     def is_valid(self):
-        return (self.hash_of_self == self.hash()) and \
-            self.transaction.verify_signature()
+        return (self.hash_of_self == self.hash())
+            # self.transaction.verify()
+
+    def proof_of_work(self):
+        return self.hash()[1:] == "0"
+
+    def mine(self):
+        while not self.proof_of_work():
+            nonce += 1
+        self.hash_of_self = self.hash()
  
 class Blockchain: 
  
     def __init__(self): 
         self.blocks = [] 
+        self.transactions = [] 
         genesis = Block("",'0') 
-        self.blocks.append(genesis) 
+        self.blocks.append(genesis)
+        self.wallet = Wallet()
+
+    def add_new_transaction(self, transaction):
+        self.transactions.append(transaction)
+
+    def get_transactions(self):
+        return self.transactions
+
+    def get_block_template(self, miner):
+        return Block(self.transactions, self.wallet.public_key, miner)
  
     def number_of_blocks(self): 
         return len(self.blocks)
@@ -81,27 +99,11 @@ class Blockchain:
                                   self.blocks[b].is_valid()
         return status and self.blocks[-1].is_valid()
 
-# Check that signing a Transaction works
-key = RSA.generate(1024) 
-public_key = key.publickey()
-key_signer = pkcs1_15.new(key)
-
-digest = SHA256.new()
-digest.update(b"Svala")
-
-signature = key_signer.sign(digest) 
-
-t = Transaction(b"Svala", public_key, signature)
-assert t.verify_signature()
-
-# Verifiy that tampering with the data is discovered
-t.data = b"Uggla"
-assert not t.verify_signature()
 
 # Check that making Transactions inside a Wallet works
 swedbank = Wallet()
-naughty_business = swedbank.make_transaction(b"Bribe politician")
-assert naughty_business.verify_signature()
+naughty_business = swedbank.make_transaction(b"Politician", 1000000)
+assert naughty_business.verify()
 
 # Check that genesis works 
 chain = Blockchain() 
@@ -119,8 +121,8 @@ assert chain.number_of_blocks() == 2
 # Check that chain is valid 
 assert chain.is_valid() 
 
-magnitskij = Wallet()
-immoral_stuff = Block(magnitskij.make_transaction(b"Pay prostitutes"),
+oligarch = Wallet()
+immoral_stuff = Block(oligarch.make_transaction(b"Prostitute", 5000),
                       chain.blocks[-1].hash_of_self)
 chain.add_block(immoral_stuff) 
 assert chain.number_of_blocks() == 3 
@@ -128,17 +130,18 @@ assert chain.number_of_blocks() == 3
 # Check that chain is valid 
 assert chain.is_valid() 
 
-dodgy_stuff = Block(swedbank.make_transaction(b"Steal money from babushkas"),
+dodgy_stuff = Block(swedbank.make_transaction(b"Babushka",
+                                              -10000),
                     chain.blocks[-1].hash_of_self)
 
 chain.add_block(dodgy_stuff)
 assert chain.number_of_blocks() == 4
 
 assert swedbank.find_my_transactions(chain) == 2
-assert magnitskij.find_my_transactions(chain) == 1
+assert oligarch.find_my_transactions(chain) == 1
 
 # Tamper with chain 
-chain.blocks[1].transaction.data = b"Totally honest stuff" 
+chain.blocks[1].transaction.receiver = b"The Pope" 
 
 # And check that it's no longer valid 
 assert not chain.is_valid()
